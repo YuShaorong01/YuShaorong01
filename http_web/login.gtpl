3fa5a4837b744862642651531a16faf5e14812dd
YuShaorong <1808506719@qq.com>
2022-10-29 16:10:08 +0800

    0

---
+++ http_web/login.gtpl
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title></title>
+</head>
+
+<body>
+<form action="/login" method="post">
+    用户名：<input type="text name=>
+</body>
+
+<html>
---
+++ http_web/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()       //解析参数，默认不会解析
+	fmt.Println(r.Form) //这些信息是输出到服务器端的打印信息
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+	fmt.Fprintf(w, "Hello astaxie!")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method) //获取请求的方法
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("login.gtpl") //解析模板
+		t.Execute(w, nil)                         //渲染模板，并发送
+	} else {
+		//请求的是登录数据，那么执行登录的逻辑判断
+		//解析表单
+		r.ParseForm()
+		fmt.Println("username:", r.Form["username"])
+		fmt.Println("password:", r.Form["password"])
+	}
+}
+func main() {
+	http.HandleFunc("/", sayhelloName)       //设置访问的路由
+	http.HandleFunc("/login", login)         //设置访问的路由
+	err := http.ListenAndServe(":9090", nil) //设置监听的端口
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
